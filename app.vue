945354c30a010700ebe72aa7f1dfb5b5bb158e5f
LyneQ <lyne.quequin@gmail.com>
2024-11-30 16:09:36 +0100

    send initial files

---
+++ app.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+// setting up meta tags and body classes
+useHead({
+  title: 'lyneq.tech',
+  meta: [
+    { name: 'description', content: 'portfolio make my myself for yourself' }
+  ],
+  bodyAttrs: {
+    class: 'light-theme'
+  },
+  script: [
+    {
+      src: 'https://kit.fontawesome.com/70805063d4.js',
+      crossorigin: "anonymous",
+      async: true,
+      defer: true
+    }
+  ]
+})
+</script>
+<template>
+  <nuxt-layout>
+    <NuxtPage/>
+  </nuxt-layout>
+</template>
+<style lang="scss">
+@use "~/assets/scss/reset.scss";
+@use "~/assets/scss/base.scss";
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "default"
+})
+</script>
+
+<template>
+  <div>
+    <h1>Default Layout</h1>
+    <NuxtPage />
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
